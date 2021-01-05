77a4dd442cdc98e6fa85791ac3c5672e93f8051b
sharky <sharmarke.ugas@outlook.com>
2021-01-05 13:22:04 +0000

    moved property data to a json , impelmented service for calls

--- Frontend/src/app/property/property-list/property-list.component.ts
+++ Frontend/src/app/property/property-list/property-list.component.ts
@@ -1,53 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { HousingService } from 'src/app/services/housing.service';
 
 @Component({
   selector: 'app-property-list',
   templateUrl: './property-list.component.html',
-  styleUrls: ['./property-list.component.css']
+  styleUrls: ['./property-list.component.css'],
 })
 export class PropertyListComponent implements OnInit {
+  properties: any;
 
-  properties : Array<any> = [{
-    "Id":1,
-    "Name":"Birla house",
-    "Type":"House",
-    "Price": 12000
-  },
-  {
-    "Id":2,
-    "Name":"Erose villa",
-    "Type":"House",
-    "Price": 14000
-  },
-  {
-    "Id":3,
-    "Name":"Penbrook estate",
-    "Type":"House",
-    "Price": 22000
-  },
-  {
-    "Id":4,
-    "Name":"Chathom house",
-    "Type":"House",
-    "Price": 40000
-  },
-  {
-    "Id":5,
-    "Name":"Ugas manner",
-    "Type":"House",
-    "Price": 92000
-  },
-  {
-    "Id":6,
-    "Name":"Pearl white house",
-    "Type":"House",
-    "Price": 18000
-  },
-]
-
-  constructor() { }
+  constructor(private housingService: HousingService) {}
 
   ngOnInit(): void {
+    this.housingService.getAllProperties().subscribe(
+      (data) => {
+        this.properties = data;
+        console.log(data);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
   }
-
 }
