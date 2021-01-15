bd9317b8db0058b9f16c76f0071567a1c9087872
sharky <sharmarke.ugas@outlook.com>
2021-01-15 15:27:44 +0000

    re-factored to reactive, updated and wired tab form/controlls

--- Frontend/src/app/model/ipropertybase.ts
+++ Frontend/src/app/model/ipropertybase.ts
@@ -2,8 +2,8 @@ export interface IPropertyBase {
   Id: number|null;
   SellRent:number|null;
   Name: string|null;
-  Ptype: string|null;
-  Ftype: string|null;
+  PType: string|null;
+  FType: string|null;
   Price: number|null;
   BHK: number|null;
   BuiltArea:number|null;
